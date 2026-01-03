f72578f5f0cf0b9c72aa4c62a40b860a9e905172
FIVE <five@FIVE.local>
2026-01-03 15:24:18 +0900

    GEMINIバージョン変更

--- api/analyze-receipt.ts
+++ api/analyze-receipt.ts
@@ -48,7 +48,7 @@ export default async function handler(
     }
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-pro-vision",
       safetySettings,
     });
 
